ffea21d7a9890d86f107f4697812c1736b11c673
Salomão Ribeiro <salomaomdm@gmail.com>
2022-09-10 17:50:46 -0300

    Adicionado projeto PartyInvites

---
+++ 03/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string? Name { get; set; }
+    }
+}
