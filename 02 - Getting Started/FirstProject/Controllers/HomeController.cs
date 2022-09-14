87e8f6d91ead20e1fc4591d3f38015e44405c258
Salomão Ribeiro <salomaomdm@gmail.com>
2022-09-14 07:03:09 -0300

    Renomeando as pastas

---
+++ 02 - Getting Started/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+    public ViewResult Index()
+    {
+        int Hours = DateTime.Now.Hour;
+        string viewModel = Hours < 12 ? "Bom dia": "Boa tarde";
+        return View("MyView", viewModel);
+    }
+}
