07c8e6af156b9c5ea814208aaf24ffd5ab620161
Salomão Ribeiro <salomaomdm@gmail.com>
2022-09-27 20:19:23 -0300

    projeto 06 - adicionado pacote Moq e mocado testes

--- 06 - Unit Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ 06 - Unit Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -2,6 +2,7 @@ using SimpleApp.Controllers;
 using SimpleApp.Models;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Xunit;
 
 
@@ -9,25 +10,40 @@ namespace SimpleApp.Tests
 {
     public class HomeControllerTests
     {
+
+        // class FakeDataSource : IDataSource
+        // {
+        //     public IEnumerable<Product> Products { get; set; }
+        //     public FakeDataSource(Product[] data) => Products = data;
+        // }
+
         [Fact]
         public void IndexActionModelIsComplete()
         {
             //Arrange
+
+            Product[] testData = new Product[]
+            {
+              new Product { Name = "P1", Price = 75.10M },
+              new Product { Name = "P2", Price = 120M },
+              new Product { Name = "P3", Price = 110M }
+            };
+
+            var mock = new Mock<IDataSource>();
+            mock.SetupGet(m => m.Products).Returns(testData);
             var controller = new HomeController();
 
-            Product[] products = new Product[]
-            {
-              new Product {Name = "Kayak", Price = 275M},
-              new Product { Name = "Life Jacket", Price = 48.95M}
-            };
+            controller.dataSource = mock.Object;
 
             //ACT
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             // Assert
-            Assert.Equal(products, model, Comparer.Get<Product>(
-                  (p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price)
+            Assert.Equal(testData, model,
+                          Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name
+                            && p1?.Price == p2?.Price)
                 );
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
--- 06 - Unit Testing/SimpleApp/Models/Product.cs
+++ 06 - Unit Testing/SimpleApp/Models/Product.cs
@@ -5,12 +5,21 @@ namespace SimpleApp.Models
         public string Name { get; set; } = String.Empty;
         public decimal? Price { get; set; }
 
-        public static Product[] GetProducts()
-        {
-            Product kayak = new Product { Name = "Kayak", Price = 275M };
-            Product lifeJacket = new Product { Name = "Life Jacket", Price = 48.95M };
+        // public static Product[] GetProducts()
+        // {
+        //     Product kayak = new Product { Name = "Kayak", Price = 275M };
+        //     Product lifeJacket = new Product { Name = "Life Jacket", Price = 48.95M };
 
-            return new Product[] { kayak, lifeJacket };
-        }
+        //     return new Product[] { kayak, lifeJacket };
+        // }
+    }
+
+    public class ProductDataSource : IDataSource
+    {
+        public IEnumerable<Product> Products =>
+          new Product[] {
+            new Product {Name = "Kayak", Price = 275M},
+            new Product { Name = "Life Jack", Price = 48.95M}
+          };
     }
 }
