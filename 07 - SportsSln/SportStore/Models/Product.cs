088aec6e7bbf9c17c09ecb01c2c3433f6824113c
Salomão Ribeiro <salomaomdm@gmail.com>
2022-10-03 07:56:40 -0300

    projeto 07 - criado as pastas base do projeto e arquivos iniciais

---
+++ 07 - SportsSln/SportStore/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportStore.Models
+{
+    public class Product
+    {
+        public long? ProductId { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        public string Category { get; set; } = String.Empty;
+    }
+}
